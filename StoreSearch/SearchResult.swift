40ee45e8de4f7bf27d2e6de7a3c1db69f3dea448
пользователь <polzovatel@macbook-pro-polzovatel.local>
2017-02-21 18:29:15 +0200

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,16 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by пользователь on 21.02.17.
+//  Copyright © 2017 Eugene Uskov. All rights reserved.
+//
+
+class SearchResult {
+ 
+    var name = ""
+    var artistName = ""
+
+
+
+}
