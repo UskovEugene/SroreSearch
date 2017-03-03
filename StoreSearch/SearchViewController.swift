3fdf497186fa20b6034fd2a42dc3f10dc111f378
пользователь <polzovatel@macbook-pro-polzovatel.local>
2017-03-03 17:32:35 +0200

    Added segmented control and ability to choose type of search result

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -19,6 +19,7 @@ class SearchViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     
     var searchResults = [SearchResult]()
     var hasSearched = false
@@ -33,13 +34,33 @@ class SearchViewController: UIViewController {
     //-----------------------------------------------------------------------------------------------
     //-----------------------------------------------------------------------------------------------
    
-    
-    func iTunesURL(searchText: String) -> URL {
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
         
+        performSearch()
+    }
+    
+    
+    
+    
+    
+    func iTunesURL(searchText: String, category: Int) -> URL {
+        
+        let entityName: String
+        
+        switch category {
+            
+        case 1: entityName = "musicTrack"
+        case 2: entityName = "software"
+        case 3: entityName = "ebook"
+        
+        default: entityName = ""
+        }
+    
         let escapedSearchText = searchText.addingPercentEncoding(
             withAllowedCharacters: CharacterSet.urlQueryAllowed)!
         
-        let urlString = String(format: "https://itunes.apple.com/search?term=%@&limit=200", escapedSearchText)
+        let urlString = String(format: "https://itunes.apple.com/search?term=%@&limit=200&entity=%@", escapedSearchText, entityName)
+        
         let url = URL(string: urlString)
         
         return url!
@@ -271,7 +292,7 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = UIEdgeInsets(top: 108, left: 0, bottom: 0, right: 0)
         tableView.rowHeight = 80
         
         var cellNib = UINib(nibName: TableViewCellIdentifiers.searchResultCell, bundle: nil)
@@ -303,7 +324,16 @@ class SearchViewController: UIViewController {
 
 extension SearchViewController: UISearchBarDelegate {
     
+   
+    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+     
+        performSearch()
+    }
+    
+    
+    
+    func performSearch() {
         
         if !searchBar.text!.isEmpty {
         
@@ -318,7 +348,9 @@ extension SearchViewController: UISearchBarDelegate {
             searchResults = []
         
 
-            let url = iTunesURL(searchText: searchBar.text!)
+            let url = iTunesURL(searchText: searchBar.text!,
+                                category: segmentedControl.selectedSegmentIndex)
+            
             let session = URLSession.shared
             
             dataTask = session.dataTask(with: url, completionHandler: {
